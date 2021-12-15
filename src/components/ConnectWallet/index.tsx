95f5864f04e53f8b9b7c3f3ebbe9cdd22daadc18
wangym <wangym0203@aliyun.com>
2021-12-15 16:10:57 +0800

    feat: remove test code

--- src/components/ConnectWallet/index.tsx
+++ src/components/ConnectWallet/index.tsx
@@ -1,10 +1,10 @@
 import ChainBridge from '@/constants/ChainBridge';
+import services from '@/services';
 import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
 import type { InjectedConnector } from '@web3-react/injected-connector';
-import { Button, notification } from 'antd';
+import { notification } from 'antd';
 import classNames from 'classnames';
 import React, { useEffect, useState } from 'react';
-import services from '@/services';
 import { injected } from './connector';
 import './index.less';
 import { useEagerConnect, useInactiveListener } from './WalletHooks';
@@ -75,31 +75,6 @@ const ConnectWallet: React.FC<IConnectWallet> = ({ typeUI, className, style, chi
               style={{ marginRight: '12px' }}
             />
             <span className="address">{`${account?.slice(0, 8)}···${account?.slice(-6)}`}</span>
-            <Button
-              onClick={async () => {
-                try {
-                  const aa = await services.evmServer.createPoolInfo(
-                    '0x08a5125c84c3dab4834a28e73a35f4b6d895e7aa',
-                    '1639037124',
-                    '1639037724',
-                    '1000000',
-                    '10000',
-                    '200000000',
-                    '0xDc6dF65b2fA0322394a8af628Ad25Be7D7F413c2',
-                    '0xF592aa48875a5FDE73Ba64B527477849C73787ad',
-                    '0xBEF7dF09BBE87487D0Aa29FB06957D48f2033D2a',
-                    '0x0A1ec9DAF76A5917C8AEa5317Bb7c27dC705eB56',
-                    '20000000',
-                  );
-                  console.log(aa);
-                } catch (err: unknown) {
-                  console.error('createPoolInfo', err);
-                }
-              }}
-            >
-              {' '}
-              Create Pool
-            </Button>
           </div>
         );
       }
