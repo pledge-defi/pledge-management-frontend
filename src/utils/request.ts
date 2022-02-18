f4f7bd954d80a815b785c5655d67ee4d5b30b26d
wangym <908289704@qq.com>
2022-02-18 15:07:34 +0800

    refactor: request token invalid

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -76,7 +76,8 @@ const errorHandler = (error: ResponseError | { response: Response }) => {
 const request = extend({
   errorHandler, // 默认错误处理
   // credentials: 'include', // 默认请求是否带上cookie
-  prefix: 'http://54.67.12.175:7005/api/v2',
+  // prefix: 'http://54.67.12.175:7005/api/v2',
+  prefix: 'https://v2-backend.pledger.finance/api/v22',
   // prefix: '/api/v2',
 });
 
@@ -92,6 +93,24 @@ request.interceptors.request.use((url, options) => {
       headers: newHeaders,
     },
   };
+});
+
+// handling error in response interceptor
+request.interceptors.response.use(async (response) => {
+  try {
+    const data = await response.clone().json();
+    if (
+      (data && data.code && data.code === 1102) ||
+      data.code === 1101 ||
+      response.status === 401
+    ) {
+      localStorage.removeItem(PLEDGE_TOKEN);
+      if (window.location.pathname !== '/users/login') {
+        history.replace('/user/login');
+      }
+    }
+  } catch (error) {}
+  return response;
 });
 
 export default request;
